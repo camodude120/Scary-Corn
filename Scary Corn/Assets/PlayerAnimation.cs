bdbdfcc32eb60a8e5e1cb8475fc2202abd80585a
dcbradleyjr <DCBradley@student.fullsail.edu>
2024-01-17 15:58:52 -0600

    Updated Animation

--- Scary Corn/Assets/PlayerAnimation.cs
+++ Scary Corn/Assets/PlayerAnimation.cs
@@ -6,21 +6,41 @@ using UnityEngine.EventSystems;
 public class PlayerAnimation : MonoBehaviour
 {
 
+    private Animator playerAnimator;
 
-    
+    public string currentState;
+    public string playerIdle = "playerIdle";
+    public string playerWalk = "playerWalk";
+    public string playerJump = "playerJump";
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerAnimator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(playerVariable.isWalking && !playerVariable.isJumping)
+        {
+            PlayAnimation(playerWalk);
+        }
+        else if(!playerVariable.isWalking && !playerVariable.isJumping)
+        {
+            PlayAnimation(playerIdle);
+        }
+        else if (playerVariable.isJumping)
+        {
+            PlayAnimation(playerJump);
+        }
     }
 
-    
+    public void PlayAnimation(string newState)
+    {
+        if (currentState == newState) return;
+        playerAnimator.Play(newState);
+        currentState = newState;
+    }
 
 }
