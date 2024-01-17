6ec062d2710f009989073e99209402174c518c39
dcbradleyjr <DCBradley@student.fullsail.edu>
2024-01-16 21:18:55 -0600

    Added Death POP

--- Scary Corn/Assets/CornScript.cs
+++ Scary Corn/Assets/CornScript.cs
@@ -6,15 +6,18 @@ using UnityEngine;
 public class CornScript : MonoBehaviour
 
 {
+    AudioSource jump;
+
     public float moveSpeed;
     public float jumpforce;
-    
+
     public Transform groundCheck;
     public LayerMask groundObjects;
     public float checkRadius;
     public int maxJumpCount;
+    public GameObject DeathExplosion;
 
-    
+
 
     private Rigidbody2D rb;
     private bool facingRight = true;
@@ -25,7 +28,7 @@ public class CornScript : MonoBehaviour
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        
+
     }
 
     private void Start()
@@ -43,38 +46,57 @@ public class CornScript : MonoBehaviour
         //Animate
         Animate();
 
-    }
-        
+        //Sound
+        jump = GetComponent<AudioSource>();
 
-       private void FixedUpdate()
-        {
-        isGrounded= Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundObjects);
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundObjects);
         if (isGrounded)
         {
             jumpCount = maxJumpCount;
         }
 
-            // Move
-            Move();
+        // Move
+        Move();
+    }
+
+    void ProcessInputs()
+    {
+        moveDirection = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump") && jumpCount > 0)
+        {
+            isJumping = true;
+        }
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Weakpoint")
+        {
+            Destroy(collision.gameObject);
         }
 
-        void ProcessInputs()
+        if (collision.gameObject.tag == "Popped")
         {
-            moveDirection = Input.GetAxis("Horizontal");
-            if (Input.GetButtonDown("Jump") && jumpCount > 0)
-            {
-                isJumping = true;
-            }
+            Dead();
         }
-    
+    }
+
 
     private void Move()
     {
         rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
-        if (isJumping && jumpCount >0 )
+        if (isJumping && jumpCount > 0)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpforce);
             jumpCount--;
+            jump.Play();
 
         }
         isJumping = false;
@@ -96,5 +118,18 @@ public class CornScript : MonoBehaviour
     {
         facingRight = !facingRight;
         transform.Rotate(0f, 180f, 0f);
+    }
+
+    private void Dead()
+    {
+        Instantiate(DeathExplosion, transform.position,Quaternion.identity);
+
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       
+
     }
 }
