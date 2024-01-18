ec013ce87b577aec1c690fb9e44a5c5a43563afd
dcbradleyjr <DCBradley@student.fullsail.edu>
2024-01-18 17:29:19 -0600

    GumDrop Bridge mobile now

--- Scary Corn/Assets/MovingPlatform.cs
+++ Scary Corn/Assets/MovingPlatform.cs
@@ -30,4 +30,14 @@ public class MovingPlatform : MonoBehaviour
 
         transform.position = Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.transform.SetParent(transform);
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        collision.transform.SetParent(null);
+    }
+
 }
